539268df49e10b2826ec9fe361f87bfbff97418b
kwilki <kylewilkinson14@gmail.com>
2022-06-06 17:07:48 +1000

    finished tests

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React, {Component} from 'react';
+import ConcatenatedModule from 'webpack/lib/optimize/ConcatenatedModule';
+
+export default class Keypad extends Component {
+
+    print = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+            <input type='password' onKeyUp={this.print} />
+        )
+    }
+}
